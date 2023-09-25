118a4400cad7abb47ed73d92f4bdd022fae5fef2
goldindollar <mister@goldindollar.com>
2023-09-24 19:30:11 -0700

    Initial Commit

---
+++ signing/signingApp.swift
@@ -0,0 +1,17 @@
+//
+//  signingApp.swift
+//  signing
+//
+//  Created by MrGold on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct signingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
